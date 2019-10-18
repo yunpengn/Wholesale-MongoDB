107d8c5d698c6fbfac3c168b2ac66b25fd2614fe
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-10-18 20:25:40 +0800

    Enable shard on table level

--- scripts/mongo-scripts/enable-shard.js
+++ scripts/mongo-scripts/enable-shard.js
@@ -1,1 +1,30 @@
+// Enables sharding on the database level.
 sh.enableSharding("wholesale");
+
+// Enables sharding on the per collection level.
+sh.shardCollection("wholesale.warehouse", {
+    w_id: 1
+});
+
+sh.shardCollection("wholesale.district", {
+    d_w_id: 1,
+    d_id: 1
+});
+
+sh.shardCollection("wholesale.customer", {
+    c_w_id: 1,
+    c_d_id: 1
+});
+
+sh.shardCollection("wholesale.order", {
+    o_w_id: 1,
+    o_d_id: 1
+});
+
+sh.shardCollection("wholesale.item", {
+    i_id: 1
+});
+
+sh.shardCollection("wholesale.stock", {
+    s_w_id: 1
+});
