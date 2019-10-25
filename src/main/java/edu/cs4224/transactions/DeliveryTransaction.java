98a574679c52e9dd51d523ee414942eb7d44ec1c
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-10-25 16:37:36 +0800

    Add delivery transaction

--- src/main/java/edu/cs4224/transactions/DeliveryTransaction.java
+++ src/main/java/edu/cs4224/transactions/DeliveryTransaction.java
@@ -5,11 +5,12 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Updates;
 
+import edu.cs4224.pojo.Customer;
 import edu.cs4224.pojo.CustomerOrder;
 import edu.cs4224.pojo.District;
+import edu.cs4224.pojo.OrderLineInfo;
 
-import java.text.Format;
-import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class DeliveryTransaction extends BaseTransaction {
   private static final int NUM_DISTRICTS = 10;
@@ -25,6 +26,7 @@ public class DeliveryTransaction extends BaseTransaction {
   }
 
   @Override public void execute(final String[] dataLines) {
+    MongoCollection<Customer> customer = Customer.getCollection(db);
     MongoCollection<CustomerOrder> order = CustomerOrder.getCollection(db);
     MongoCollection<District> district = District.getCollection(db);
 
@@ -32,7 +34,38 @@ public class DeliveryTransaction extends BaseTransaction {
       District currentDistrict = district.findOneAndUpdate(Filters.and(
           Filters.eq("d_W_ID", warehouseID),
           Filters.eq("d_ID", i)
-      ), Updates.set("d_NEXT_DELIVERY_O_ID", 1));
+      ), Updates.inc("d_NEXT_DELIVERY_O_ID", 1));
+      if (currentDistrict == null) {
+        throw new RuntimeException(String.format("Unable to find district with warehouseID=%d districtID=%d", warehouseID, i));
+      }
+
+      int orderID = currentDistrict.getD_NEXT_DELIVERY_O_ID();
+      CustomerOrder yetDeliverOrder = order.find(Filters.and(
+          Filters.eq("o_W_ID", warehouseID),
+          Filters.eq("o_D_ID", i),
+          Filters.eq("o_ID", orderID)
+      )).first();
+      if (yetDeliverOrder == null) {
+        throw new RuntimeException(String.format("Unable to find order with warehouseID=%d districtID=%d orderID=%d", warehouseID, i, orderID));
+      }
+
+      yetDeliverOrder.setO_CARRIER_ID(carrierID);
+      double totalAmount = 0;
+      for (OrderLineInfo orderLine: yetDeliverOrder.getO_L_INFO().values()) {
+        orderLine.setOL_DELIVERY_D(new Date());
+        totalAmount += orderLine.getOL_AMOUNT();
+      }
+
+      customer.updateOne(Filters.and(
+          Filters.eq("c_W_ID", warehouseID),
+          Filters.eq("c_d_ID", i),
+          Filters.eq("c_ID", yetDeliverOrder.getO_C_ID())
+      ), Updates.inc("c_BALANCE", totalAmount));
+      customer.updateOne(Filters.and(
+          Filters.eq("c_W_ID", warehouseID),
+          Filters.eq("c_d_ID", i),
+          Filters.eq("c_ID", yetDeliverOrder.getO_C_ID())
+      ), Updates.inc("c_DELIVERY_CNT", 1));
     }
   }
 }
