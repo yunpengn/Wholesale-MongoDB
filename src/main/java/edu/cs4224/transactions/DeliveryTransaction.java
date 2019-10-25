b52234d8c74b90a8ea22fde0b51f59aa825d7d49
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-10-25 16:14:35 +0800

    Use ONE_THREE

--- src/main/java/edu/cs4224/transactions/DeliveryTransaction.java
+++ src/main/java/edu/cs4224/transactions/DeliveryTransaction.java
@@ -1,6 +1,12 @@
 package edu.cs4224.transactions;
 
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+
+import edu.cs4224.pojo.CustomerOrder;
+import edu.cs4224.pojo.District;
 
 import java.text.Format;
 import java.text.SimpleDateFormat;
@@ -19,6 +25,14 @@ public class DeliveryTransaction extends BaseTransaction {
   }
 
   @Override public void execute(final String[] dataLines) {
+    MongoCollection<CustomerOrder> order = CustomerOrder.getCollection(db);
+    MongoCollection<District> district = District.getCollection(db);
 
+    for (int i = 1; i <= NUM_DISTRICTS; i++) {
+      District currentDistrict = district.findOneAndUpdate(Filters.and(
+          Filters.eq("d_W_ID", warehouseID),
+          Filters.eq("d_ID", i)
+      ), Updates.set("d_NEXT_DELIVERY_O_ID", 1));
+    }
   }
 }
