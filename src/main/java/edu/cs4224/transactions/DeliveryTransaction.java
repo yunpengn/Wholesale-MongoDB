2b71dc6f6885fcef87d8d7d11022bef9d77674dd
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-11-06 16:30:38 +0800

    Separate update

--- src/main/java/edu/cs4224/transactions/DeliveryTransaction.java
+++ src/main/java/edu/cs4224/transactions/DeliveryTransaction.java
@@ -68,7 +68,12 @@ public class DeliveryTransaction extends BaseTransaction {
           Filters.eq("c_W_ID", warehouseID),
           Filters.eq("c_d_ID", i),
           Filters.eq("c_ID", yetDeliverOrder.getO_C_ID())
-      ), Updates.combine(Updates.inc("c_BALANCE", totalAmount), Updates.inc("c_DELIVERY_CNT", 1)));
+      ), Updates.inc("c_BALANCE", totalAmount));
+      customer.updateOne(Filters.and(
+          Filters.eq("c_W_ID", warehouseID),
+          Filters.eq("c_d_ID", i),
+          Filters.eq("c_ID", yetDeliverOrder.getO_C_ID())
+      ), Updates.inc("c_DELIVERY_CNT", 1));
     }
   }
 }
