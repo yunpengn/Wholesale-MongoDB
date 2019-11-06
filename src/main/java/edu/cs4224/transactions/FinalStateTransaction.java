7620b087afac589f190393df3c3d3b07c70b8c44
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-11-06 15:44:38 +0800

    Add sum

--- src/main/java/edu/cs4224/transactions/FinalStateTransaction.java
+++ src/main/java/edu/cs4224/transactions/FinalStateTransaction.java
@@ -1,14 +1,22 @@
 package edu.cs4224.transactions;
 
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Accumulators;
+import com.mongodb.client.model.Aggregates;
+
+import edu.cs4224.pojo.Warehouse;
+
+import java.util.Collections;
+import java.util.function.Consumer;
 
 public class FinalStateTransaction extends BaseTransaction {
-
   public FinalStateTransaction(final MongoDatabase db, final String[] parameters) {
     super(db, parameters);
   }
 
   @Override public void execute(final String[] dataLines) {
-
+    Warehouse.getCollection(db).aggregate(Collections.singletonList(
+        Aggregates.group("$stars", Accumulators.sum("w_YTD", 1))
+    )).forEach((Consumer<Warehouse>) record -> System.out.println(record.toString()));
   }
 }
